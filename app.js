5413d049d07428877192567a56e9b1606f25f9d4
Mike Marlow <michael.s.marlow@gmail.com>
2018-01-23 14:10:38 -0700

    yup

---
+++ app.js
@@ -0,0 +1,48 @@
+//grab data from form input
+  //store name and append to page HTML
+
+//POST to server
+
+//Render from server data (if it exists)
+
+//Player Card details
+  //name
+  //order in queue
+  //remove button ... onClick, remove from page;
+
+
+let playersContainer = document.querySelector('.player-list');
+
+
+document.querySelector('.add').addEventListener("click", function(event) {
+  event.preventDefault();
+  
+})
+
+
+function getNameFromForm() {
+  const data = new FormData(document.querySelector('form'));
+  let name = data.get("playerName");
+  return {
+    player: name
+  };
+}
+
+function injectable(name) {
+  `<div class="">
+    <h1 class="name">${name}</h1>
+    <button type="button" class="remove btn btn-outline-danger">DELETE</button>
+  </div>`
+}
+
+function postToServer() {
+  return fetch(url, {
+    method: 'POST',
+    headers: new Headers({
+      "Content-Type": "application/json"
+    }),
+    body: JSON.stringify(getNameFromForm())
+  }).then(res => res.json())
+  .then(res => res.message)
+  .catch(console.error);
+}
